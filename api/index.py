652f12eee4cb9a07effbcbe43e2b32872216d767
Yuki <86597897+Yuki-1018@users.noreply.github.com>
2025-07-03 12:11:22 +0900

    Update index.py

--- api/index.py
+++ api/index.py
@@ -3,7 +3,7 @@ from pytube import YouTube
 import os
 
 # Flaskアプリケーションの設定
-app = Flask(__name__, template_folder="../templates")
+app = Flask(__name__, template_folder="templates")
 
 @app.route("/", methods=["GET", "POST"])
 def index():
